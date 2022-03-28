7ef98ccc079f4115bbe5e3956a0bc54619081a9c
hugosenari <hugosenari@gmail.com>
2022-03-27 21:55:29 -0300

    chore: re index ptest

--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-ptest-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-ptest-v0_1_0.owner = "treeform";
   inputs.src-ptest-v0_1_0.repo  = "ptest";
-  inputs.src-ptest-v0_1_0.dir   = "";
   inputs.src-ptest-v0_1_0.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
